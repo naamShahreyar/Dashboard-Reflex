aaf1c96570a2ff71b94daab806e5708476a43813
naamShahreyar <shahreyar1411@gmail.com>
2024-12-28 23:20:22 +0530

    Form Added

--- DashBoard/DashBoard.py
+++ DashBoard/DashBoard.py
@@ -4,35 +4,118 @@ import reflex as rx
 
 from rxconfig import config
 
+class User(rx.Base):
+    """The user model."""
+
+    name: str
+    email: str
+    gender: str
+
 
 class State(rx.State):
-    """The app state."""
+    users: list[User] = [
+        User(
+            name="Danilo Sousa",
+            email="danilo@example.com",
+            gender="Male",
+        ),
+        User(
+            name="Zahra Ambessa",
+            email="zahra@example.com",
+            gender="Female",
+        ),
+    ]
 
-    ...
+    def add_user(self, form_data: dict):
+        self.users.append(User(**form_data))
+
+
+def show_user(user: User):
+    """Show a person in a table row."""
+    return rx.table.row(
+        rx.table.cell(user.name),
+        rx.table.cell(user.email),
+        rx.table.cell(user.gender),
+    )
+
+
+def add_customer_button() -> rx.Component:
+    return rx.dialog.root(
+        rx.dialog.trigger(
+            rx.button(
+                rx.icon("plus", size=26),
+                rx.text("Add User", size="4"),
+            ),
+        ),
+        rx.dialog.content(
+            rx.dialog.title(
+                "Add New User",
+            ),
+            rx.dialog.description(
+                "Fill the form with the user's info",
+            ),
+            rx.form(
+                rx.flex(
+                    rx.input(
+                        placeholder="User Name",
+                        name="name",
+                        required=True,
+                    ),
+                    rx.input(
+                        placeholder="user@reflex.dev",
+                        name="email",
+                    ),
+                    rx.select(
+                        ["Male", "Female"],
+                        placeholder="Male",
+                        name="gender",
+                    ),
+                    rx.flex(
+                        rx.dialog.close(
+                            rx.button(
+                                "Cancel",
+                                variant="soft",
+                                color_scheme="gray",
+                            ),
+                        ),
+                        rx.dialog.close(
+                            rx.button(
+                                "Submit", type="submit"
+                            ),
+                        ),
+                        spacing="3",
+                        justify="end",
+                    ),
+                    direction="column",
+                    spacing="4",
+                ),
+                on_submit=State.add_user,
+                reset_on_submit=False,
+            ),
+            max_width="450px",
+        ),
+    )
 
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+    return rx.vstack(
+        add_customer_button(),
+        rx.table.root(
+            rx.table.header(
+                rx.table.row(
+                    rx.table.column_header_cell("Name"),
+                    rx.table.column_header_cell("Email"),
+                    rx.table.column_header_cell("Gender"),
+                ),
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
+            rx.table.body(
+                rx.foreach(State.users, show_user),
             ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
+            variant="surface",
+            size="3",
         ),
-        rx.logo(),
     )
+
 
 
 app = rx.App()
